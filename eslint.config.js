0c61bc949b0c8cf82fc5b75760d698c051490e19
Pietro <pietro.dcg@gmail.com>
2026-01-26 02:38:48 -0300

    build: install eslint-plugin-react

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import plugin from "eslint-plugin-react";
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -12,6 +13,7 @@ export default defineConfig([
       js.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      plugin.react.recommended,
     ],
     languageOptions: {
       ecmaVersion: 2020,
